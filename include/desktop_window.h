9b1945fa34d4b5cdf6b789f64b499028c02d2182
WarrenWu4 <warrenweiwu04@gmail.com>
2025-09-01 17:54:36 -0500

    feat: added all drawing steps

--- include/desktop_window.h
+++ include/desktop_window.h
@@ -4,12 +4,32 @@
 #include <vector>
 #include <string>
 
+struct Point {
+    int x;
+    int y;
+};
+
 class DesktopWindow {
     private:
         HINSTANCE hInstance;
         HWND hwnd = nullptr;
         static LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
+        void DrawContainer(HDC hdc);
+        void DrawCloseButton(HDC hdc);
+        void DrawDesktopNames(HDC hdc);
+        void DrawDraggableButton(HDC hdc);
+
+        std::vector<std::wstring> desktopNames;
+        int currentDesktopIndex;
+
+        RECT closeRect = {};
+        RECT finalDesktopRect = {};
+        RECT dragRect = {};
+        Point containerPadding = {10, 8};
+        Point textPadding = {8, 4};
+        int borderRadius = 8;
+        int gap = 8;
     public:
         DesktopWindow(HINSTANCE hInstance);
         ~DesktopWindow();
--- src/desktop_window.cpp
+++ src/desktop_window.cpp
@@ -20,7 +20,7 @@ DesktopWindow::DesktopWindow(HINSTANCE hInstance) : hInstance(hInstance) {
         nullptr, // parent window
         nullptr, // menu
         hInstance, // instance handle
-        nullptr // additional application data
+        this // additional application data
     );
     // SetWindowLong(hwnd, GWL_EXSTYLE, GetWindowLong(hwnd, GWL_EXSTYLE) | WS_EX_LAYERED);
     // SetLayeredWindowAttributes(hwnd, 0, 255, LWA_ALPHA); // adjust transparency 0-255
@@ -29,6 +29,9 @@ DesktopWindow::DesktopWindow(HINSTANCE hInstance) : hInstance(hInstance) {
     }
 
     // closeIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_CLOSE_ICON));
+
+    desktopNames = { L"Testing", L"testing2" };
+    currentDesktopIndex = 0;
 }
 
 DesktopWindow::~DesktopWindow() {
@@ -37,9 +40,16 @@ DesktopWindow::~DesktopWindow() {
     }
 }
 
-void drawContainer(HWND hwnd, HDC hdc) {
-    RECT rect;
-    GetClientRect(hwnd, &rect);
+void DesktopWindow::DrawContainer(HDC hdc) {
+    RECT parent;
+    GetClientRect(hwnd, &parent);
+    // RECT rect = {
+    //     parent.left,
+    //     parent.top,
+    //     dragRect.right+containerPadding.x,
+    //     parent.bottom
+    // };
+    RECT rect = parent;
     HBRUSH brush = CreateSolidBrush(RGB(24, 24, 37));
     HPEN hPen = CreatePen(PS_NULL, 0, RGB(0, 0, 0));
     HBRUSH oldBrush = (HBRUSH)SelectObject(hdc, brush);
@@ -51,33 +61,106 @@ void drawContainer(HWND hwnd, HDC hdc) {
     DeleteObject(hPen);
 }
 
-void drawCloseButton(HINSTANCE hInstance, HDC hdc) {
+void DesktopWindow::DrawDesktopNames(HDC hdc) {
+    // grab position of parent and calculate next child position
+    RECT parent;
+    GetClientRect(hwnd, &parent);
+    int rectX = closeRect.right + gap;
+    int rectY = closeRect.top;
+
+    // set text parameters
+    SetTextColor(hdc, RGB(135, 141, 164));
+    SetBkMode(hdc, TRANSPARENT);
+
+    // iterate through desktop names and draw each
+    for (size_t i = 0; i < desktopNames.size(); i++) {
+        const std::wstring& name = desktopNames[i];
+        SIZE size;
+        GetTextExtentPoint32(hdc, name.c_str(), name.length(), &size);
+        RECT textRect = { rectX, rectY, rectX+size.cx+2*textPadding.x, closeRect.bottom };
+        HBRUSH brush = CreateSolidBrush(RGB(30, 30, 46));
+        HPEN pen = CreatePen(PS_NULL, 0, RGB(0, 0, 0));
+        HBRUSH oldBrush = (HBRUSH)SelectObject(hdc, brush);
+        HPEN oldPen = (HPEN)SelectObject(hdc, pen);
+        RoundRect(hdc, textRect.left, textRect.top, textRect.right, textRect.bottom, borderRadius, borderRadius);
+        SelectObject(hdc, oldBrush);
+        SelectObject(hdc, oldPen);
+        DeleteObject(brush);
+        DeleteObject(pen);
+        DrawText(hdc, name.c_str(), -1, &textRect, DT_CENTER | DT_VCENTER | DT_SINGLELINE);
+        rectX = textRect.right + gap;
+    }
+    finalDesktopRect = {rectX, rectY, rectX, rectY };
+}
+
+void DesktopWindow::DrawCloseButton(HDC hdc) {
+    // calculate position relative to parent
+    RECT parent;
+    GetClientRect(hwnd, &parent);
+    int rectX = parent.left+containerPadding.x;
+    int rectY = parent.top+containerPadding.y;
+
+    // create and draw new rect
+    int rectWidth = 32;
+    int rectHeight = 32;
+    closeRect = {rectX, rectY, rectX+rectWidth, rectY+rectHeight};
+    HBRUSH brush = CreateSolidBrush(RGB(30, 30, 46));
+    HPEN pen = CreatePen(PS_NULL, 0, RGB(0, 0, 0));
+    HBRUSH oldBrush = (HBRUSH)SelectObject(hdc, brush);
+    HPEN oldPen = (HPEN)SelectObject(hdc, pen);
+    RoundRect(hdc, closeRect.left, closeRect.top, closeRect.right, closeRect.bottom, borderRadius, borderRadius);
+    SelectObject(hdc, oldBrush);
+    SelectObject(hdc, oldPen);
+    DeleteObject(brush);
+    DeleteObject(pen);
+
+    // create and draw close icon
     HICON closeIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_CLOSE_ICON));
-    ICONINFO iconInfo;
-    GetIconInfo(closeIcon, &iconInfo);
-    BITMAP bm;
-    GetObject(iconInfo.hbmColor, sizeof(BITMAP), &bm);
-    int iconWidth = bm.bmWidth;
-    int iconHeight = bm.bmHeight;
-    DeleteObject(iconInfo.hbmColor);
-    DeleteObject(iconInfo.hbmMask);
-    int iconX = 10+(32-iconWidth)/2;
-    int iconY = 8+(32-iconHeight)/2;
+    int iconWidth = 16;
+    int iconHeight = 16;
+    int iconX = rectX+(rectWidth-iconWidth)/2;
+    int iconY = rectY+(rectHeight-iconHeight)/2;
     DrawIcon(hdc, iconX, iconY, closeIcon);
 }
 
+void DesktopWindow::DrawDraggableButton(HDC hdc) {
+    RECT parent;
+    GetClientRect(hwnd, &parent);
+    int rectX = finalDesktopRect.right;
+    int rectY = parent.top+containerPadding.y;
+    int rectWidth = 32;
+    dragRect = {rectX, rectY, rectX+rectWidth, rectY+rectWidth};
+    HBRUSH brush = CreateSolidBrush(RGB(30, 30, 46));
+    HPEN pen = CreatePen(PS_NULL, 0, RGB(0, 0, 0));
+    HBRUSH oldBrush = (HBRUSH)SelectObject(hdc, brush);
+    HPEN oldPen = (HPEN)SelectObject(hdc, pen);
+    RoundRect(hdc, dragRect.left, dragRect.top, dragRect.right, dragRect.bottom, borderRadius, borderRadius);
+    SelectObject(hdc, oldBrush);
+    SelectObject(hdc, oldPen);
+    DeleteObject(brush);
+    DeleteObject(pen);
+}
+
 LRESULT CALLBACK DesktopWindow::WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
-    RECT closeRect = {370, 10, 390, 30};
+    DesktopWindow* pThis = nullptr;
+    if (uMsg == WM_NCCREATE) {
+        CREATESTRUCT *pCreate = reinterpret_cast<CREATESTRUCT *>(lParam);
+        pThis = reinterpret_cast<DesktopWindow *>(pCreate->lpCreateParams);
+        SetWindowLongPtr(hwnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pThis));
+        pThis->hwnd = hwnd;
+    } else {
+        pThis = reinterpret_cast<DesktopWindow*>(GetWindowLongPtr(hwnd, GWLP_USERDATA));
+    }
     switch (uMsg) {
         case WM_LBUTTONDOWN: {
             POINT pt;
             GetCursorPos(&pt);
             ScreenToClient(hwnd, &pt);
-            if (pt.x < 30) {
+            if (PtInRect(&pThis->dragRect, pt)) {
                 ReleaseCapture();
                 SendMessage(hwnd, WM_NCLBUTTONDOWN, HTCAPTION, 0);
             }
-            if (PtInRect(&closeRect, pt)) {
+            if (PtInRect(&pThis->closeRect, pt)) {
                 SendMessage(hwnd, WM_CLOSE, 0, 0);
             }
             break;
@@ -85,7 +168,10 @@ LRESULT CALLBACK DesktopWindow::WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, 
         case WM_PAINT: {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hwnd, &ps);
-            drawContainer(hwnd, hdc);
+            pThis->DrawContainer(hdc);
+            pThis->DrawCloseButton(hdc);
+            pThis->DrawDesktopNames(hdc);
+            pThis->DrawDraggableButton(hdc);
             EndPaint(hwnd, &ps);
             break;
         }
